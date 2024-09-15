561cdb024333bb031993890196be43fa953c3509
Pamendra <pamendrasinghdhurwe@gmail.com>
2024-09-15 12:47:57 +0530

    Initial commit

---
+++ lib/changes/strings.dart
@@ -0,0 +1,28 @@
+String hellotag = '''Hi there, Welcome to My Space  ''';
+
+String yourname = "I'm Pamendra Singh Dhurwe,";
+// String hometitle2 = "A Mobile Application\nDeveloper";
+
+String animationtxt1 = " Mobile Application Developer";
+// String animationtxt2 = " UI/UX Designer";
+String animationtxt3 = " Web Developer";
+
+//
+String contactHeadding = "Let’s try my service now!";
+String contactSubHeadding =
+    "Let’s work together and make everything super cute and super useful.";
+//
+String miniDescription =
+    "Freelancer providing services for programming and design content needs. Join me down below and let's get started!";
+
+String servicesSubHeading =
+    "Since the beginning of my journey as a freelance designer and developer, I've worked in startups and collaborated with talented people to create digital products for both business and consumer use. I offer a wide range of services, including brand design, programming and teaching.";
+//
+
+String protfolioSubHeading =
+    "Since the beginning of my journey as a mobile application developer, I have created digital products for business and consumer use. This is a little bit.";
+const String aboutMeHeadline =
+    'I\'m Pamendra Singh Dhurwe, a Mobile App developer, Web developer and UI designer.';
+
+const String aboutMeDetail =
+    'I have done my Masters in Computer Application (MCA) from Shri Shankaracharaya Technical Campus Bhilai, CG. I have been developing mobile apps for over 1.2 years now. Currently working as an Project Assistant (Mobile App Developer) at IIT Bhilai.';
